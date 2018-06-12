d1df499cad73a388122b7b3f1c51faa7d56ca9f2
Haroen Viaene <hello@haroen.me>
2018-06-12 19:32:43 +0200

    simplify

--- src/screens/searchBar/index.vue
+++ src/screens/searchBar/index.vue
@@ -1,23 +1,53 @@
 <template>
   <nb-container :style="{ backgroundColor: '#fff' }">
-        <nb-header searchBar rounded>
-          <nb-item>
-            <nb-icon active name="search" />
-            <nb-input placeholder="Search" />
-            <nb-icon active name="people" />
-          </nb-item>
-          <nb-button transparent>
-            <nb-text>Search</nb-text>
-          </nb-button>
-        </nb-header>
-        <nb-content padder>
-           <nb-button
-            block
-            :onPress="() => this.props.navigation.navigate('DrawerOpen')"
-          >
-            <Text>Back</Text>
-          </nb-button>
-        </nb-content>
-      </nb-container>
+    <ais-index
+      app-id="latency"
+      api-key="3d9875e51fbd20c7754e65422f7ce5e1"
+      index-name="bestbuy"
+    >
+      <nb-header searchBar rounded>
+        <nb-item>
+          <nb-icon active name="search" />
+          <ais-search-box />
+          <nb-icon active name="people" />
+        </nb-item>
+        <nb-button transparent>
+          <nb-text>Search</nb-text>
+        </nb-button>
+      </nb-header>
+      <nb-content padder>
+        <ais-hits></ais-hits>
+
+        <nb-button
+          block
+          :onPress="() => this.props.navigation.navigate('DrawerOpen')"
+        >
+          <Text>Back</Text>
+        </nb-button>
+      </nb-content>
+    </ais-index>
+  </nb-container>
 </template>
 
+<script>
+import Hits from './ais-hits.vue'
+import SearchBox from './ais-search-box.vue'
+import Index from './ais-index.vue'
+
+export default {
+  components: {
+    'ais-index': Index,
+    'ais-search-box': SearchBox,
+    'ais-hits': Hits,
+    },
+  data() {
+    return {
+      items: [
+        {objectID: 1, name: "hi"},
+        {objectID: 2, name: "hi 2"},
+        {objectID: 3, name: "hi 3"},
+      ]
+    }
+  },
+}
+</script>
